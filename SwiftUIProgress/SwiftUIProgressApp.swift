f121e3ba6a82b90c2e478df82a07b40cddbc2965
Masataka Nakagawa <n.masataka.hal.ih@gmail.com>
2020-12-18 16:25:34 +0900

    Initial Commit

---
+++ SwiftUIProgress/SwiftUIProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProgressApp.swift
+//  SwiftUIProgress
+//
+//  Created by Masataka Nakagawa on 2020/12/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
